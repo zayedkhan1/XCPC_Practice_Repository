540ff5386b5ff92254fcc08c0a52643d2bf2afc0
zayed khan <zayedk458@gmail.com>
2025-09-02 21:58:41 +0600

    Second Max of Three Numbers solved

---
+++ Second_Max_of_Three_Numbers.py
@@ -0,0 +1,9 @@
+t=int(input())
+for _ in range(t):
+    a,b,c=map(int,input().split())
+    if(a>b and a<c) or (a>c and a<b):
+        print(a)
+    elif(b>a and b<c) or (b>c and b<a):
+        print(b)
+    else:
+        print(c)
